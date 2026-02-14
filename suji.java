7cba3b63c33e39b634516cc1544907cc56da0d99
suthesh-V <sutheshsuji23@gmail.com>
2026-02-14 18:27:45 +0530

    Add files via upload

---
+++ suji.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class suji {
+    public static void main(String args[]){
+        Scanner re = new Scanner(System.in);
+        int count =0;
+        do{
+            System.out.println("Enter no. > 10:");
+            count = re.nextInt();
+        }while(count<10);
+        }
+}
